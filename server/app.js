51063bb47f2b027da012d9c72ac5a03925427a57
nadhiljanitra <nadhiljanitraa@gmail.com>
2019-10-04 18:50:27 +0700

    initial commit

---
+++ server/app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const routes = require('./routes')
+const app = express()
+const PORT = process.env.PORT || 3000
+
+
+app.use('/',routes)
+
+
+
+
+
+app.listen(PORT,()=>'listening on ',PORT)
