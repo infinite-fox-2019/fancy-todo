f1a2b4f733e820e78d115cf3703a12b94bd78160
TigorLazuardi <tigor.hutasuhut@gmail.com>
2019-10-25 09:24:02 +0700

    up

--- server/bin/https.js
+++ server/bin/https.js
@@ -3,9 +3,11 @@ const app = require('../app')
 const fs = require('fs')
 const PORT = process.env.SECURE_PORT || 443
 
+const domain = 'c-todo.crowfx.xyz'
+
 const option = {
-    cert: fs.readFileSync('/etc/letsencrypt/live/c-todo.crowfx.xyz/fullchain.pem'),
-    key: fs.readFileSync('/etc/letsencrypt/live/c-todo.crowfx.xyz/privkey.pem')
+    cert: fs.readFileSync('/etc/letsencrypt/live/' + domain + '/fullchain.pem'),
+    key: fs.readFileSync('/etc/letsencrypt/live/' + domain + '/privkey.pem')
 }
 
 const server = https.createServer(option, app)
